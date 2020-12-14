bff877336ad6ef450a00723dfa0b458633fff292
Cynthia Kitili <nyambucindy0@gmail.com>
2020-12-14 11:04:01 +0300

    add log out function

--- app/auth/views.py
+++ app/auth/views.py
@@ -30,4 +30,10 @@ def register():
         flash('Your account has been created! You are now able to log in', 'success')
         return redirect(url_for('auth.login'))
         title = "New Account"
-    return render_template('auth/register.html', registration_form=form)    
+    return render_template('auth/register.html', registration_form=form)   
+
+@auth.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.home'))     

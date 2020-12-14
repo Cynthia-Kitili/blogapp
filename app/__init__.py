5551b007823542de6e2ca16ffded7c0737f0afc1
Cynthia Kitili <nyambucindy0@gmail.com>
2020-12-14 12:26:58 +0300

    add elif to request username

--- app/__init__.py
+++ app/__init__.py
@@ -44,3 +44,46 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('main.home'))
+
+#creating an account and picture save function
+@users.route("/account", methods=['GET', 'POST'])
+@login_required
+def account():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('users.account'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account',image_file=image_file, form=form) 
+
+#Displaying the posts for an author
+@users.route("/user/<string:username>")
+def user_posts(username):
+    page = request.args.get('page',1,type=int)
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = Post.query.filter_by(author=user)\
+        .order_by(Post.date_posted.desc())\
+        .paginate(page=page, per_page=5)
+    return render_template('user_posts.html', posts=posts, user=user)
+
+#Reset password function
+@users.route("/reset_password", methods=['GET', 'POST'])
+def reset_request(): 
+    if current_user.is_authenticated:
+        return redirect(url_for('main.home'))  
+    form = RequestResetForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+        send_reset_email(user)
+        flash('An email has been sent with instructions to reset your password.', 'info')
+        return redirect(url_for('users.login')) 
+    return render_template('reset_request.html', title='Reset Password', form=form) 
